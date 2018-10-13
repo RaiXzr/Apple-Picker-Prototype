0cc1a4264de8417cce700b4832b199057e783b73
RaiXzr <41179919+RaiXzr@users.noreply.github.com>
2018-10-14 01:04:24 +1100

    Initial commit

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+
+    void Awake()
+    {// 1
+     // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {// 2
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);// 3
+    }
+
+	void Update ()
+    {
+        TextMeshProUGUI gt = this.GetComponent<TextMeshProUGUI>();
+        gt.text = "High Score:" + score;
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {// 4
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+    }
+}
